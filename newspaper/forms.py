5ed763597b640588a5baddc8b79582ee2c6a1189
Alex <AlexGrech98@gmail.com>
2023-04-09 20:04:57 +0300

    new

--- newspaper/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Post
-
-
-class NewsForm(ModelForm):
-    class Meta:
-        model = Post
-        fields = ['categoryType', 'title', 'authorArticle', 'text']
